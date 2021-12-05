af50df4ff48d1d75533c2e418a07c534d3fa5138
fred.fu <fjw08199999@gmail.com>
2021-12-05 09:08:50 +0800

    Initial Commit

---
+++ choiceAPP_V1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  choiceAPP_V1
+//
+//  Created by fred fu on 2021/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
